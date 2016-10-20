5be9a8e75eab48637324e25bf9a1494093d6b1cd
Hamid Raza <hamidabdul1994@hotmail.com>
2016-10-20 16:33:39 +0530

    --

--- fundooHR end/controller/firebaseCtrl.js
+++ fundooHR end/controller/firebaseCtrl.js
@@ -3,13 +3,14 @@
  */
 var express = require('express');
 var router = express.Router();
-  var firebase=require('../config/firebase.js');
+var firebase=require('../config/firebase.js');
 
 router.get('/save', function(req, res) {
 
   console.log("Firebase called....");
   var ref = firebase.database().ref();
    ref.on("value", function(snapshot) {
+     res.send(snapshot.val());
        console.log(snapshot.val());
    }, function(error) {
        console.log("Error: " + error.code);
@@ -27,7 +28,6 @@ router.get('/update', function(req, res) {
           "lastName":"Doe"
         }
     });
-
     //console.log(firebase+"Data save...");
 });
 
